1bfbbdf2ffbe54bac80aa736e0b00c35a02e083b
Mohammad Al Samiul <alsamiul123@gmail.com>
2025-09-02 21:22:07 +0600

    product controllers all functionality added

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,4 +1,56 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
+import { ProductsService } from './products.service';
+import { ProductDto } from './dto/product.dto';
+import { JwtAuthGuard } from 'src/common/guards/jwt-auth.guard';
+import { RolesGuard } from 'src/common/guards/roles.guard';
+import { Role } from 'src/common/enums/roles.enum';
+import { Roles } from 'src/common/decorators/roles.decorator';
 
 @Controller('products')
-export class ProductsController {}
+export class ProductsController {
+  constructor(private productsService: ProductsService) {}
+
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(Role.Admin)
+  @Post()
+  createProduct(@Body() productDto: ProductDto) {
+    return this.productsService.createProduct(productDto);
+  }
+
+  @Get()
+  getAllProducts() {
+    return this.productsService.getAllProducts();
+  }
+
+  @Get(':id')
+  getOneProduct(@Param('id', ParseIntPipe) id: number) {
+    return this.productsService.getOneProduct(id);
+  }
+
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(Role.Admin)
+  @Patch(':id')
+  updateProduct(
+    @Body() productDto: ProductDto,
+    @Param('id', ParseIntPipe) id: number,
+  ) {
+    return this.productsService.updateProduct(id, productDto);
+  }
+
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(Role.Admin)
+  @Delete(':id')
+  deleteProduct(@Param('id', ParseIntPipe) id: number) {
+    return this.productsService.deleteProduct(id);
+  }
+}
