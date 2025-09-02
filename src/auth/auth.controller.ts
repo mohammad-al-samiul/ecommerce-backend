a5840f08531f4e23dc3db699c4972b65acc0cd34
Mohammad Al Samiul <alsamiul123@gmail.com>
2025-09-02 12:17:47 +0600

    login and register controller added

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { RegisterDto } from './dto/register.dto';
+import { LoginDto } from './dto/login.dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authService: AuthService) {}
+  @Post('register')
+  registerUser(@Body() registerDto: RegisterDto) {
+    return this.authService.registerUser(registerDto);
+  }
+
+  @Post('login')
+  loginUser(loginDto: LoginDto) {
+    return this.authService.loginUser(loginDto);
+  }
+}
