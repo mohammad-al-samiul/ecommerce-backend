f9f33154892ab2955c3e185b395e8870a48d12c7
Mohammad Al Samiul <alsamiul123@gmail.com>
2025-09-03 11:19:01 +0600

    cart enitity added

---
+++ src/carts/entiies/cart.entity.ts
@@ -0,0 +1,25 @@
+import { Product } from 'src/products/entities/product.entity';
+import { User } from 'src/users/entities/user.entity';
+import {
+  Column,
+  Entity,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  Unique,
+} from 'typeorm';
+
+@Entity()
+@Unique(['user', 'product'])
+export class Cart {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @ManyToOne(() => User, { eager: true, onDelete: 'CASCADE' })
+  user: User;
+
+  @ManyToOne(() => Product, { eager: true, onDelete: 'CASCADE' })
+  product: Product;
+
+  @Column({ default: 1 })
+  quantity: number;
+}
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,33 @@
+/* eslint-disable @typescript-eslint/no-floating-promises */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
 import { ValidationPipe } from '@nestjs/common';
-async function bootstrap() {
-  dotenv.config();
-  const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
-  app.setGlobalPrefix('api');
-  await app.listen(process.env.PORT ?? 3000);
+import { Server } from 'http';
+
+dotenv.config();
+
+let cachedServer: Server;
+
+export default async function bootstrap(): Promise<Server> {
+  if (!cachedServer) {
+    const app = await NestFactory.create(AppModule);
+    app.useGlobalPipes(
+      new ValidationPipe({ whitelist: true, transform: true }),
+    );
+    app.setGlobalPrefix('api');
+    await app.init(); // <-- For Vercel, use init() instead of listen()
+    cachedServer = app.getHttpAdapter().getInstance();
+  }
+  return cachedServer;
 }
-bootstrap();
+
+// Call bootstrap locally for development
+if (process.env.NODE_ENV !== 'production') {
+  bootstrap().then((app) => {
+    const port = process.env.PORT ?? 3000;
+    app.listen(port, () => {
+      console.log(`🚀 Server running on http://localhost:${port}`);
+    });
+  });
+}
